64341ff45c6e9600b0398d83de582afd3b1fe48e
nizanhulq <nizandhiaulhaq@mail.ugm.ac.id>
2023-12-22 10:44:45 +0700

    feat : make page notFound and cardNews

---
+++ src/pages/eLearning/index.jsx
@@ -0,0 +1,14 @@
+import CardNews from "@/components/card/news";
+import Layout from "@/components/layout";
+
+const ELearning = () => {
+    return (
+        <Layout >
+            <div className="m-8">
+                <CardNews />
+            </div>
+        </Layout>
+    );
+};
+
+export default ELearning;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,6 +7,8 @@ import { useEffect } from "react";
 
 import Index from "@/pages/home";
 import Login from "@/pages/auth/login";
+import NotFound from "@/pages/notFound";
+import ELearning from "@/pages/eLearning";
 
 
 export default function Router() {
@@ -19,6 +21,14 @@ export default function Router() {
     {
       path: "/auth/login",
       element: <Login />,
+    },
+    {
+      path: "/e-learning",
+      element: <ELearning />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
     },
   ]);
 
