5b5af8038378ca7a0174e3e38d3b3238076a38f4
nizanhulq <nizandhiaulhaq@mail.ugm.ac.id>
2023-12-22 21:49:12 +0700

    feat : add card learnign path

--- src/pages/eLearning/index.jsx
+++ src/pages/eLearning/index.jsx
@@ -1,11 +1,25 @@
+import CardLearning from "@/components/card/learningPath";
 import CardNews from "@/components/card/news";
 import Layout from "@/components/layout";
 
+import learningImg from "@/assets/images/learning-path-1.png";
+
 const ELearning = () => {
+    const dataLearning = {
+        title: "Digital Marketing",
+        image: learningImg,
+        material: 109,
+        topic: 5,
+        users: 100,
+        rating: 4
+
+    }
+
     return (
         <Layout >
-            <div className="m-8">
+            <div className="m-8 space-y-4">
                 <CardNews />
+                <CardLearning learning={dataLearning} />
             </div>
         </Layout>
     );
