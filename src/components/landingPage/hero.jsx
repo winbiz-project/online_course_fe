65e5102985bb25264a6ac09e730fe4d762f906d0
nizanhulq <nizandhiaulhaq@mail.ugm.ac.id>
2024-01-30 10:18:09 +0700

    feat: ganti warna hero di landing page

--- src/components/landingPage/hero.jsx
+++ src/components/landingPage/hero.jsx
@@ -5,7 +5,13 @@ import { FaAngleRight } from "react-icons/fa6";
 const Hero = () => {
   return (
     <Box width={"100%"} height={"100%"} position="relative">
-      <Image src={bg_hero} width={"100%"} height={"100%"} objectFit="cover" filter={"blur(5px)"} />
+      <Image
+        src={bg_hero}
+        width={"100%"}
+        height={"100%"}
+        objectFit="cover"
+        filter={"blur(5px)"}
+      />
 
       <Flex
         position="absolute"
@@ -14,7 +20,12 @@ const Hero = () => {
         transform="translate(-80%, -50%)"
         flexDirection={"column"}
       >
-        <Text fontSize="6xl" color="black" fontWeight={"semibold"} className="drop-shadow-lg">
+        <Text
+          fontSize="6xl"
+          color="white"
+          fontWeight={"semibold"}
+          className="drop-shadow-lg"
+        >
           Belajar Fleksibel Ratusan Skill. Dapatkan Sertifikat.{" "}
         </Text>
         <Text fontSize="3xl" color={"white"} className="drop-shadow-2xl">
@@ -22,7 +33,13 @@ const Hero = () => {
           terstruktur, modul praktik plus webinar series rancangan para expert
           dari top companies.{" "}
         </Text>
-        <Button mt={4} bgColor={"#1450A3"} color={"white"} size="lg" width={"fit-content"}>
+        <Button
+          mt={4}
+          bgColor={"#1450A3"}
+          color={"white"}
+          size="lg"
+          width={"fit-content"}
+        >
           Lihat Ratusan Materi Tersedia <FaAngleRight className="ms-3" />
         </Button>
       </Flex>
