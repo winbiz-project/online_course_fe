59c5be4b857e58846f71676095b7583d46050d7a
Mohamad Fariz Kurniawan <mohamad.fariz@ui.ac.id>
2024-08-13 11:08:22 +0700

    feat: changing Text as='b' to fontWeight={"bold"} and justify margin

--- src/pages/terms/index.jsx
+++ src/pages/terms/index.jsx
@@ -15,17 +15,17 @@ const Terms = () => {
             <Text fontSize="lg" mb={4}>
                 Ketentuan Penggunaan ini mengatur penggunaan Anda atas situs web, konten, serta fitur pada Skillbridge. Harap baca Ketentuan ini dengan cermat, dan hubungi kami jika Anda memiliki pertanyaan, permintaan informasi, atau keluhan. Dengan mengakses dan menggunakan situs Skillbridge, berarti Anda telah memahami dan menyetujui untuk terikat dan tunduk dengan semua peraturan yang berlaku di situs ini.
             </Text>
-            <Text as="b" fontSize="2xl" mb={4}>
+            <Text fontWeight={"bold"} fontSize="2xl" mb={2}>
                 1. Layanan yang Ditawarkan
             </Text>
             <Text fontSize="lg" mb={4}>
                 Skillbridge menyediakan kursus online yang dirancang untuk mengembangkan keahlian profesional dalam berbagai bidang. Kami berhak untuk mengubah, memperbarui, atau menghentikan layanan kami kapan saja tanpa pemberitahuan terlebih dahulu.
             </Text>
-            <Text as="b" fontSize="2xl" mb={4}>
+            <Text fontWeight={"bold"} fontSize="2xl" mb={2}>
                 2. Akun Pengguna
             </Text>
             <Box>
-                <Text as="b" fontSize="xl" mb={4}>
+                <Text fontWeight={"bold"} fontSize="xl" mb={2}>
                     2.1 Pendaftaran
                 </Text>
                 <Text fontSize="lg" mb={4}>
@@ -33,18 +33,18 @@ const Terms = () => {
                 </Text>
             </Box>
             <Box>
-                <Text as="b" fontSize="xl" mb={4}>
+                <Text fontWeight={"bold"} fontSize="xl" mb={2}>
                     2.2 Keamanan Akun
                 </Text>
                 <Text fontSize="lg" mb={4}>
                     Anda bertanggung jawab untuk menjaga kerahasiaan informasi akun Anda, termasuk kata sandi. Anda setuju untuk segera memberitahu kami tentang setiap penggunaan tidak sah atas akun Anda.
                 </Text>
             </Box>
-            <Text as="b" fontSize="2xl" mb={4}>
+            <Text fontWeight={"bold"} fontSize="2xl" mb={2}>
                 3. Pengunaan Konten
             </Text>
             <Box>
-                <Text as="b" fontSize="xl" mb={4}>
+                <Text fontWeight={"bold"} fontSize="xl" mb={2}>
                     3.1 Hak Cipta
                 </Text>
                 <Text fontSize="lg" mb={4}>
@@ -52,18 +52,18 @@ const Terms = () => {
                 </Text>
             </Box>
             <Box>
-                <Text as="b" fontSize="xl" mb={4}>
+                <Text fontWeight={"bold"} fontSize="xl" mb={2}>
                     3.2 Lisensi Pengguna
                 </Text>
                 <Text fontSize="lg" mb={4}>
                     Kami memberikan Anda lisensi terbatas, tidak eksklusif, tidak dapat dialihkan, dan dapat dicabut untuk mengakses dan menggunakan konten kursus kami untuk penggunaan pribadi dan non-komersial.
                 </Text>
             </Box>
-            <Text as="b" fontSize="2xl" mb={4}>
+            <Text fontWeight={"bold"} fontSize="2xl" mb={2}>
                 4. Pembayaran dan Langganan
             </Text>
             <Box>
-                <Text as="b" fontSize="xl" mb={4}>
+                <Text fontWeight={"bold"} fontSize="xl" mb={2}>
                     4.1 Harga dan Pembayaran
                 </Text>
                 <Text fontSize="lg" mb={4}>
@@ -71,7 +71,7 @@ const Terms = () => {
                 </Text>
             </Box>
             <Box>
-                <Text as="b" fontSize="xl" mb={4}>
+                <Text fontWeight={"bold"} fontSize="xl" mb={2}>
                     4.2 Kebijakan Pengembalian Dana
                 </Text>
                 <Text fontSize="lg" mb={4}>
@@ -84,13 +84,13 @@ const Terms = () => {
                     Biaya berlangganan, biaya transaksi, dan biaya lainnya yang sudah dibayarkan tidak dapat ditukar, dibatalkan, dikembalikan, diganti, atau ditransfer ke pihak lain.
                 </Text>
             </Box>
-            <Text as="b" fontSize="2xl" mb={4}>
+            <Text fontWeight={"bold"} fontSize="2xl" mb={2}>
                 5. Kode Etik Pengguna
             </Text>
             <Text fontSize="lg" mb={2}>
                 Anda setuju untuk tidak menggunakan platform ini untuk melakukan hal-hal berikut:
             </Text>
-            <UnorderedList spacing={3} fontSize="lg" mb={4}>
+            <UnorderedList spacing={3} fontSize="lg" mb={2}>
             <ListItem>
                 Mengunggah, memposting, atau mengirimkan konten yang melanggar hukum, menyinggung, atau melanggar hak kekayaan intelektual orang lain.
             </ListItem>
@@ -101,31 +101,31 @@ const Terms = () => {
                 Menggunakan Skillbridge dalam kondisi atau cara apapun yang dapat merusak, melumpuhkan, membebani, atau mengganggu server atau jaringan Skillbridge. Anda juga tidak diperbolehkan untuk mengakses layanan, akun pengguna, sistem komputer atau jaringan secara tidak sah, dengan cara hacking, password mining, atau cara tidak sah lainnya.
             </ListItem>
             </UnorderedList>
-            <Text as="b" fontSize="2xl" mb={4}>
+            <Text fontWeight={"bold"} fontSize="2xl" mb={2}>
                 6. Pembatasan Tanggung Jawab
             </Text>
             <Text fontSize="lg" mb={4}>
                 Kami tidak bertanggung jawab atas kerugian atau kerusakan yang mungkin timbul dari penggunaan situs kami atau ketidakmampuan untuk mengakses situs kami. Kami juga tidak menjamin bahwa situs kami akan bebas dari kesalahan atau gangguan.
             </Text>
-            <Text as="b" fontSize="2xl" mb={4}>
+            <Text fontWeight={"bold"} fontSize="2xl" mb={2}>
                 7. Perubahan Syarat dan Ketentuan
             </Text>
             <Text fontSize="lg" mb={4}>
                 Kami berhak untuk mengubah syarat dan ketentuan ini kapan saja. Perubahan akan diberlakukan segera setelah diposting di situs web. Penggunaan berkelanjutan Anda atas situs setelah perubahan menandakan persetujuan Anda terhadap syarat dan ketentuan yang diperbarui.
             </Text>
-            <Text as="b" fontSize="2xl" mb={4}>
+            <Text fontWeight={"bold"} fontSize="2xl" mb={2}>
                 8. Hukum yang Berlaku
             </Text>
             <Text fontSize="lg" mb={4}>
                 Syarat dan ketentuan ini diatur oleh dan ditafsirkan sesuai dengan hukum yang berlaku di Indonesia. Setiap sengketa yang timbul dari atau terkait dengan syarat dan ketentuan ini akan diselesaikan di pengadilan yang berwenang di Jakarta, Indonesia.
             </Text>
-            <Text as="b" fontSize="2xl" mb={4}>
+            <Text fontWeight={"bold"} fontSize="2xl" mb={2}>
                 9. Hubungi Kami
             </Text>
-            <Text fontSize="lg" mb={4}>
+            <Text fontSize="lg" mb={2}>
                 Jika Anda memiliki pertanyaan mengenai Ketentuan Layanan ini, Anda dapat menghubungi Kami melalui:
             </Text>
-            <HStack spacing={4}>
+            <HStack spacing={2}>
                 <Text fontSize="lg">WhatsApp :</Text>
                 <Link href="https://wa.me/6282295345875">
                     <HStack>
