cbd52c096fa4619acdbe8320b883734d3048dbbf
Mohamad Fariz Kurniawan <mohamad.fariz@ui.ac.id>
2024-12-02 09:52:24 +0700

    feature: Change Privacy Policy page language to English

--- src/pages/privacy/index.jsx
+++ src/pages/privacy/index.jsx
@@ -10,153 +10,153 @@ const Privacy = () => {
                 Kebijakan Privasi
             </Heading>
             <Text fontSize="lg" mb={4}>
-                PT D&W Internasional ( <Text as={'span'} fontWeight={'bold'}>"kami"</Text> atau <Text as={'span'} fontWeight={'bold'}> "Skillbridge"</Text>) berkomitmen untuk melindungi dan menghormati privasi Anda. Kebijakan Privasi ini menjelaskan bagaimana kami mengumpulkan, menggunakan, dan melindungi informasi pribadi Anda ketika Anda mengakses dan menggunakan situs web Skillbridge.
+                Skillbridge is committed to protecting and respecting your privacy. This Privacy Policy explains how we collect, use, and safeguard your personal information when you access and use the Skillbridge website.
             </Text>
             <Text fontSize='lg' mb={4}>
-                Anda diwajibkan untuk membaca Ketentuan Layanan dan Kebijakan Privasi dengan cermat sebelum mulai menggunakan atau mengakses situs web kami. Semua ketentuan dalam Kebijakan Privasi ini berlaku dan mengikat bagi pengguna situs web. Dengan mengunjungi, mendaftar, atau mengakses akun di situs web, Anda dianggap menerima dan menyetujui seluruh Ketentuan Layanan dan Kebijakan Privasi.
+                You are required to carefully read the Terms of Service and Privacy Policy before using or accessing our website. All provisions in this Privacy Policy apply to and are binding on website users. By visiting, registering, or accessing an account on the website, you are considered to have accepted and agreed to all Terms of Service and the Privacy Policy.
             </Text>
             <Text fontWeight={"bold"} fontSize="2xl" mb={2}>
-                1. Informasi yang Kami Kumpulkan
+                1. Information We Collect
             </Text>
             <Box>
                 <Text fontWeight={"bold"} fontSize="xl" mb={2}>
-                    1.1 Informasi yang Anda Berikan
+                    1.1 Information You Provide
                 </Text>
                 <Text fontSize="lg" mb={1}>
-                    Kami dapat mengumpulkan informasi yang Anda berikan secara langsung kepada kami, seperti saat Anda mendaftar untuk sebuah akun, berlangganan kursus, mengisi formulir, atau berkomunikasi dengan kami. Informasi ini mungkin termasuk:
+                    We may collect information that you provide directly to us, such as when you register for an account, subscribe to courses, fill out forms, or communicate with us. This information may include:
                 </Text>
                 <UnorderedList spacing={1} fontSize="lg" mb={2}>
                 <ListItem>
-                    Nama lengkap
+                    Full name
                 </ListItem>
                 <ListItem>
-                    Alamat email
+                    Email address
                 </ListItem>
                 <ListItem>
-                    Nomor telepon
+                    Phone number
                 </ListItem>
                 <ListItem>
-                    Informasi pembayaran
+                    Payment information
                 </ListItem>
                 <ListItem>
-                    Informasi lainnya yang Anda pilih untuk berikan
+                    Other information you choose to provide
                 </ListItem>
                 </UnorderedList>
             </Box>
             <Box>
                 <Text fontWeight={"bold"} fontSize="xl" mb={2}>
-                    1.2 Informasi yang Dikumpulkan Secara Otomatis
+                    1.2 Information Collected Automatically
                 </Text>
                 <Text fontSize="lg" mb={1}>
-                    Kami juga dapat mengumpulkan informasi tertentu secara otomatis ketika Anda mengunjungi situs kami, termasuk:
+                    We may also collect certain information automatically when you visit our site, including:
                 </Text>
                 <UnorderedList spacing={1} fontSize="lg" mb={2}>
                 <ListItem>
-                    Alamat IP
+                    IP address
                 </ListItem>
                 <ListItem>
-                    Jenis browser
+                    Browser type
                 </ListItem>
                 <ListItem>
-                    Halaman yang Anda kunjungi
+                    Pages you visit
                 </ListItem>
                 <ListItem>
-                    Waktu dan tanggal kunjungan
+                    Date and time of your visit
                 </ListItem>
                 <ListItem>
-                    Aktivitas penelusuran lainnya
+                    Other browsing activities
                 </ListItem>
                 </UnorderedList>
             </Box>
             <Text fontSize='lg' mb={4}>
-                Informasi ini digunakan untuk memberikan statistik umum tentang penggunaan situs web atau aplikasi.
+                This information is used to provide general statistics about the use of the website or application.
             </Text>
             <Text fontWeight={"bold"} fontSize="2xl" mb={2}>
-                2. Penggunaan Informasi
+                2. How We Use Your Information
             </Text>
             <Text fontSize="lg" mb={2}>
-                Kami menggunakan informasi yang kami kumpulkan untuk berbagai tujuan, termasuk:
+                We use the information we collect for various purposes, including:
             </Text>
             <UnorderedList spacing={3} fontSize="lg" mb={2}>
             <ListItem>
-                Menyediakan dan mengelola layanan kami
+                Providing and managing our services
             </ListItem>
             <ListItem>
-                Memproses transaksi dan mengirimkan konfirmasi
+                Processing transactions and sending confirmations
             </ListItem>
             <ListItem>
-                Mengelola akun Anda dan menyediakan layanan pelanggan
+                Managing your account and providing customer service
             </ListItem>
             <ListItem>
-                Mengirimkan komunikasi pemasaran, jika Anda memilih untuk menerimanya
+                Sending marketing communications, if you choose to receive them
             </ListItem>
             <ListItem>
-                Meningkatkan situs web dan layanan kami
+                Improving our website and services
             </ListItem>
             <ListItem>
-                Mematuhi kewajiban hukum dan peraturan
+                Complying with legal and regulatory obligations
             </ListItem>
             </UnorderedList>
             <Text fontWeight={"bold"} fontSize="2xl" mb={2}>
-                3. Berbagi Informasi
+                3. Sharing Your Information
             </Text>
             <Text fontSize="lg" mb={2}>
-                Kami tidak akan membagikan informasi pribadi Anda kepada pihak ketiga kecuali dalam situasi berikut:
+                We will not share your personal information with third parties except in the following situations:
             </Text>
             <UnorderedList spacing={3} fontSize="lg" mb={2}>
             <ListItem>
-                Dengan persetujuan Anda
+                With your consent
             </ListItem>
             <ListItem>
-                Untuk mematuhi kewajiban hukum
+                To comply with legal obligations
             </ListItem>
             <ListItem>
-                Dengan penyedia layanan yang bekerja atas nama kami
+                With service providers working on our behalf
             </ListItem>
             <ListItem>
-                Untuk melindungi hak, kepemilikan, atau keamanan Skillbridge, pengguna kami, atau publik
+                To protect the rights, property, or safety of Skillbridge, our users, or the public
             </ListItem>
             </UnorderedList>
             <Text fontWeight={"bold"} fontSize="2xl" mb={2}>
-                4. Perlindungan dan Keamanan Informasi
+                4. Protection and Security of Information
             </Text>
             <Text fontSize="lg" mb={2}>
-                Kami berkomitmen untuk melindungi kerahasiaan dan keamanan Data Pribadi Anda menggunakan langkah-langkah keamanan fisik, teknis, dan administratif sesuai standar industri. Kami tidak akan membagikan Data Pribadi Anda dengan pihak ketiga kecuali dinyatakan dalam Pemberitahuan Privasi ini atau jika diperlukan dalam situasi khusus, seperti ancaman fisik terhadap Anda atau orang lain, sesuai hukum yang berlaku. Namun, karena Internet bukanlah lingkungan yang sepenuhnya aman, kami tidak dapat menjamin keamanan absolut Data Pribadi Anda. Ada risiko bahwa pihak ketiga yang tidak berwenang dapat menghindari sistem keamanan kami atau mencegat transmisi informasi Anda melalui Internet. Anda bertanggung jawab untuk melindungi keamanan informasi login Anda.
+                We are committed to maintaining the confidentiality and security of your Personal Data using physical, technical, and administrative security measures in line with industry standards. We will not share your Personal Data with third parties unless stated in this Privacy Notice or required under special circumstances, such as physical threats to you or others, as permitted by applicable law. However, since the Internet is not a completely secure environment, we cannot guarantee the absolute security of your Personal Data. There is a risk that unauthorized third parties may circumvent our security systems or intercept your information during transmission over the Internet. You are responsible for safeguarding the security of your login information.
             </Text>
             <Text fontWeight={"bold"} fontSize="2xl" mb={2}>
-                5. Hak-Hak Anda
+                5. Your Rights
             </Text>
             <Text fontSize="lg" mb={2}>
-                Anda memiliki hak untuk:
+                You have the right to:
             </Text>
             <UnorderedList spacing={3} fontSize="lg" mb={2}>
             <ListItem>
-                Mengakses informasi pribadi yang kami miliki tentang Anda
+                Access the personal information we hold about you
             </ListItem>
             <ListItem>
-                Memperbaiki informasi yang tidak akurat atau tidak lengkap
+                Correct inaccurate or incomplete information
             </ListItem>
             <ListItem>
-                Meminta penghapusan informasi pribadi Anda
+                Request the deletion of your personal information
             </ListItem>
             <ListItem>
-                Menolak pemrosesan informasi pribadi Anda untuk tujuan tertentu
+                Object to the processing of your personal information for specific purposes
             </ListItem>
             <ListItem>
-                Meminta pembatasan pemrosesan informasi pribadi Anda
+                Request restrictions on the processing of your personal information
             </ListItem>
             </UnorderedList>
             <Text fontWeight={"bold"} fontSize="2xl" mb={2}>
-                6. Perubahan Kebijakan Privasi 
+                6. Updates to This Privacy Policy
             </Text>
             <Text fontSize="lg" mb={2}>
-                Kami dapat memperbarui Kebijakan Privasi ini dari waktu ke waktu. Setiap perubahan akan diposting di halaman ini dengan tanggal pembaruan yang terbaru. Kami mendorong Anda untuk meninjau Kebijakan Privasi ini secara berkala untuk tetap mendapatkan informasi tentang bagaimana kami melindungi informasi pribadi Anda.
+                We may update this Privacy Policy from time to time. Any changes will be posted on this page with the latest update date. We encourage you to review this Privacy Policy periodically to stay informed about how we protect your personal information.
             </Text>
             <Text fontWeight={"bold"} fontSize="2xl" mb={2}>
-                7. Hubungi Kami 
+                7. Contact Us
             </Text>
             <Text fontSize="lg" mb={2}>
-            Jika Anda memiliki pertanyaan, permintaan informasi, atau keluhan mengenai Kebijakan Privasi ini, atau jika Anda ingin mendapatkan informasi untuk kerjasama atau mengalami kendala, silakan hubungi kami di:
+                If you have any questions, information requests, or complaints regarding this Privacy Policy, or if you want to inquire about collaborations or face any issues, please contact us via:
             </Text>
             <HStack spacing={2}>
                 <Text fontSize="lg">WhatsApp :</Text>
